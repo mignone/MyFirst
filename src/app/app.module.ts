23371a4099da7774d2ba076c56a9c8e2642ae234
Viszlai Viktória <viktoriaviszlai@gmail.com>
2020-08-22 23:22:47 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyButtonComponent } from './my-button/my-button.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyButtonComponent
+    MyButtonComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
